291ba5496f1daa6c4a2fc42d39186271c23269e6
thanh4890 <thanh4890@gmail.com>
2019-01-03 00:18:36 +0700

    Testing for POST /todos

--- mongodb-app/app.js
+++ mongodb-app/app.js
@@ -1,23 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
 const mongoose = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
 
-const otherTodo = new Todo({
-  text: ' 123 abc  '
+var app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  })
+
+  todo.save().then(todo => {
+    res.send({todo})
+  }, e => {
+    res.status(400).send()
+  })
 })
 
-otherTodo.save().then(doc => {
-  console.log('Saved todo', JSON.stringify(doc, null, 2))
-}, e => {
-  console.log(e)
+app.listen(3000, () => {
+  console.log('Started on port 3000')
 })
 
-const newUser = new User({
-  email: 'john@doe.com'
-})
-
-newUser.save().then(doc => {
-  console.log(JSON.stringify(doc, null, 2))
-}, e => {
-  console.log(e)
-})
+module.exports = {app}
