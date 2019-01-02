abfe2da3d7c378103213c5bf1f67905e5515e7d8
thanh4890 <thanh4890@gmail.com>
2019-01-02 23:20:02 +0700

    validator, default, user model

--- mongodb-app/app.js
+++ mongodb-app/app.js
@@ -5,13 +5,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true})
 
 const Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 })
 
@@ -26,14 +31,31 @@ const Todo = mongoose.model('Todo', {
 //   console.log('Unable to save todo')
 // })
 
-const otherTodo = new Todo({
-  text: 'Other todo',
-  completed: 'false',
-  completedAt: '123'
+// const otherTodo = new Todo({
+//   text: ' 123 abc  '
+// })
+
+// otherTodo.save().then(doc => {
+//   console.log('Saved todo', JSON.stringify(doc, null, 2))
+// }, e => {
+//   console.log(e)
+// })
+
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
 })
 
-otherTodo.save().then(doc => {
-  console.log('Saved todo', doc)
+const newUser = new User({
+  email: 'john@doe.com'
+})
+
+newUser.save().then(doc => {
+  console.log(JSON.stringify(doc, null, 2))
 }, e => {
   console.log(e)
 })
